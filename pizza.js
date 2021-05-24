ba727df5f02ce05ac01ac20052031698c5c34261
DillonGrabskiInquiries <sahasrahlainquiries@gmail.com>
2021-05-24 11:56:59 -0500

    ya

---
+++ pizza.js
@@ -0,0 +1,10 @@
+function pizzaOven(crustType, sauceType, cheeses, toppings) {
+    var pizza = {}
+    pizza.crustType = "Deep Dish"
+    pizza.sauceType = "Traditional"
+    pizza.cheeses = ["mozzarella"]
+    pizza.toppings = ["pepperoni", "sausage"]
+    return pizza
+}
+
+console.log(pizzaOven())
